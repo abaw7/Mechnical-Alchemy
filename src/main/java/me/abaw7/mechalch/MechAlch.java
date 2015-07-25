9d0925c984b4a50c42c57938736026bf9fb726e9
Abaw Seven <abaw007@gmail.com>
2015-07-24 23:21:10 -0700

    First setup

---
+++ src/main/java/me/abaw7/mechalch/MechAlch.java
@@ -0,0 +1,18 @@
+package me.abaw7.mechalch;
+
+import cpw.mods.fml.common.Mod;
+
+/**
+ * Created by Abaw7 on 7/24/2015.
+ * Licensing information included in accompanied License.txt file
+ * If License.txt file is no included with this class file, licensing terms are as below:
+ * Copyright Abaw7 All Rights Reserved
+ * Rights are granted to the owners of the domain known as Ecz.org.uk and tcreborn.co to Use this file as they see fit
+ * on the condition that they do not distribute the source code of this class file, or the compiled data.
+ */
+@Mod(modid = MechAlch.MODID, name = MechAlch.MODNAME, version = MechAlch.VERSION)
+public class MechAlch {
+    public static final String MODID = "Mechacnical Alchemy";
+    public static final String MODNAME = "Mechacnical Alchemy";
+    public static final String VERSION = "@VERSION@";
+}
